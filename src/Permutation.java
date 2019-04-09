554593290e07918b65cc4854d4ed91a12477f8df
u6555407 <u6555407@anu.edu.au>
2019-04-10 01:48:02 +1000

    Permutations:Non working permutation method

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Permutation extends  GPA {
     /*TODO:
@@ -17,7 +18,37 @@ public class Permutation extends  GPA {
 
 
    public List getPermutation(){
-       return new ArrayList();
+       int sumOfStack=0;
+       int points=0;
+       int[] trackers={0,0,0,0};
+       int[] gpa ={7,6,5,4};
+       Stack<Integer> stack=new Stack();
+       ArrayList permutations=new ArrayList();
+       stack.addElement(trackers[0]);
+       while(!stack.empty()){
+           if(stack.size()==4){
+               stack.addElement(numOfCourses-sumOfStack); //calculating remaining left over
+               if(totalPointsReqd==points){
+                   permutations.add(stack.toArray()); //Converting  stack to array for the sake of the test , can be optimised later
+               }
+               sumOfStack-=stack.pop()-stack.pop();// 5th  & 4th element being removed from stack & subtracted at the same time
+           }
+           else{
+               int val=trackers[stack.size()];
+               if(val!=numOfCourses-sumOfStack+1){//+1 so to consider permutation which has zeros in it
+                   points+=val*gpa[stack.size()];//updating points in accordance
+                   sumOfStack+=val;  //updating sumOfStack
+                   stack.addElement(val);
+                   trackers[stack.size()]++;
+               }
+               else{
+                   trackers[stack.size()]=0;
+                   stack.pop();// or just remove if there's a function available
+               }
+
+           }
+       }
+        return permutations;
    }
 
 

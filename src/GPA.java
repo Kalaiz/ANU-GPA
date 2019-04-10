277a10cfa51162f5bfcd08ac13b4c5537f434cd9
Jared Graf <u5953599@anu.edu.au>
2019-04-10 16:00:46 +1000

    "completed gpa for now"

--- src/GPA.java
+++ src/GPA.java
@@ -9,45 +9,81 @@ public class GPA {
      * 2)Implement gpaCalc
      */
 
-    int totalPointsReqd;
     int gradeValues[] = {7,6,5,4,0};
-    int numOfTBTCourses;//num of courses yet To Be Taken (TBT)
     //All instance variable below are required for totalPointsReqd
+    int numOfTBTCourses;//num of courses yet To Be Taken (TBT)
     int numOfTCourses;//num of courses Taken already
     float cgpa;//Current GPA
-    float estdGPA;
     private boolean calc;//states whether there needs to be any calculation done
+    //the below variables are related to the calculate total points function
+    float gpaWanted;
+    int pointsNeeded;
 
-
-    public void calculateTotalPoints(){
-        /*Calculates Total Points required based on numOfTCourses & cgpa */
-        int currentPoints = (int) ((cgpa*numOfTCourses)+0.5);
-        totalPointsReqd = (int)(estdGPA*(numOfTCourses+numOfTBTCourses)+0.5)-currentPoints;
-        
-    }
-
-
-    public GPA() {
+    public GPA(int[] grades, int totalCourses) {
         //default constructor
+    	gpaCalc(grades);
+    	//total courses is chosen for ease of use- more likely to know how 
+    	//many in total rather than whats left
+    	numOfTBTCourses = totalCourses - numOfTCourses;
+    }
+    public GPA(float gpa) {
+        //secondary constructor constructor
+    	cgpa = gpa;
     }
     
-    public static void main(String[] args) {
-	}
+    
+    public void calculateTotalPoints(float wantedGPA){
+        /*Calculates Total Points required based on numOfTCourses & cgpa */
+        int currentPoints = (int) ((cgpa*numOfTCourses)+0.5);
+        pointsNeeded = (int)(wantedGPA*(numOfTCourses+numOfTBTCourses)+0.5)-currentPoints;
+        gpaWanted = wantedGPA;
+    }
 
-    public float gpaCalc(int[] data) {
+    public void gpaCalc(int[] data) {
+    	//calculates the gpa and number of courses taken from your grades
     	float total = 0;
     	int tCourses = 0;
     	for (int x = 0; x < 5; x++) {
     		total = total + (data[x]*gradeValues[x]);
-    		//the below line may not be neccessary, was added to make sure 
-    		//gpacalc worked without the balue numOfTCources. can be changed
     		tCourses = tCourses+ data[x];
     	}
-        return (total/tCourses);
+        cgpa = total/tCourses;
+        numOfTCourses = tCourses;
     }
-
-
-
-
-
+    
+    //below are the 5 functions to obtain a useful value from the gpa object
+    //just in case we decided to make the values private. can delete later
+    public float getCGPA() {
+    	return cgpa;
+    }
+    
+    public float getgpaWanted() {
+    	return gpaWanted;
+    }
+    
+    public int getTBTCourses() {
+    	return numOfTBTCourses;
+    }
+    
+    public int getTCourses() {
+    	return numOfTCourses;
+    }
+    
+    public int getPointsReq() {
+    	return pointsNeeded;
+    }
+    
+    //three functions to set the values of a gpa object. the other two arent
+    //needed as there should be no reason to set those manually
+    public void setCGPA(float gpa) {
+    	cgpa = gpa;
+    }
+    
+    public void setTBTCourses(int courseNum) {
+    	numOfTBTCourses = courseNum;
+    }
+    
+    public void setTCourses(int courseNum) {
+    	numOfTCourses = courseNum;
+    }
 }

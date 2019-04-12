8d08f1bf7d755e9b20299f65fe2a1ef4e6c14aaf
u6555407 <u6555407@anu.edu.au>
2019-04-12 17:27:12 +1000

    Commiting to checkout branch

--- src/GPA.java
+++ src/GPA.java
@@ -11,9 +11,10 @@ public class GPA {
 
     final int gradeValues[] = {7,6,5,4,0};
     //All instance variable below are required for totalPointsReqd
-    int numOfTBTCourses;//num of courses yet To Be Taken (TBT)
+    int numOfCourses;//total num of courses
     int numOfTCourses;//num of courses Taken already
     float cgpa;//Current GPA
+    int currentPoints;//current number of grade points
     private boolean calc;//states whether there needs to be any calculation done
     //the below variables are related to the calculate total points function
     float gpaWanted;
@@ -21,71 +22,84 @@ public class GPA {
 
     public GPA(int[] grades, int totalCourses) {
         //default constructor
-    	gpaCalc(grades);
-    	//total courses is chosen for ease of use- more likely to know how 
-    	//many in total rather than whats left
-    	numOfTBTCourses = totalCourses - numOfTCourses;
+        gpaCalc(grades);
+        //total courses is chosen for ease of use- more likely to know how
+        //many in total rather than whats left
+        numOfCourses = totalCourses - numOfTCourses;
     }
     public GPA(float gpa, int coursesDone, int totalCourses) {
-        //secondary constructor constructor
-    	cgpa = gpa;
-    	numOfTCourses = coursesDone;
-    	numOfTBTCourse = totalCourses;
+        //secondary constructor
+        cgpa = gpa;
+        numOfTCourses = coursesDone;
+        numOfCourses = totalCourses;
     }
-    
-    
+
     public void calculateTotalPoints(float wantedGPA){
         /*Calculates Total Points required based on numOfTCourses & cgpa */
-        int currentPoints = (int) ((cgpa*numOfTCourses)+0.5);
-        pointsNeeded = (int)(wantedGPA*(numOfTCourses+numOfTBTCourses)+0.5)-currentPoints;
+        pointsNeeded = (int)(wantedGPA*(numOfCourses)+0.5)-currentPoints;
         gpaWanted = wantedGPA;
     }
 
     public void gpaCalc(int[] data) {
-    	//calculates the gpa and number of courses taken from your grades
-    	float total = 0;
-    	int tCourses = 0;
-    	for (int x = 0; x < 5; x++) {
-    		total = total + (data[x]*gradeValues[x]);
-    		tCourses = tCourses+ data[x];
-    	}
+        //calculates the gpa and number of courses taken from your grades
+        float total = 0;
+        int tCourses = 0;
+        for (int x = 0; x < 5; x++) {
+            total = total + (data[x]*gradeValues[x]);
+            tCourses = tCourses+ data[x];
+        }
         cgpa = total/tCourses;
+        currentPoints = (int)(total+0.5);
         numOfTCourses = tCourses;
     }
-    
+
+    //the below functions are update functions, used to changes a saved gpa object
+    public void addGrades(int[] newGrades) {
+        int extraClasses = 0;
+        int extraPoints = 0;
+        for(int x = 0; x <5; x++) {
+            extraClasses = extraClasses +newGrades[x];
+            extraPoints = extraPoints + (newGrades[x]*gradeValues[x]);
+        }
+        cgpa = (cgpa*numOfTCourses+extraPoints)/(numOfTCourses + extraClasses);
+        numOfTCourses = numOfTCourses + extraClasses;
+    }
+
+
+
     //below are the 5 functions to obtain a useful value from the gpa object
     //just in case we decided to make the values private. can delete later
     public float getCGPA() {
-    	return cgpa;
+        return cgpa;
     }
-    
+
     public float getgpaWanted() {
-    	return gpaWanted;
+        return gpaWanted;
     }
-    
-    public int getTBTCourses() {
-    	return numOfTBTCourses;
+
+    public int getCourses() {
+        return numOfCourses;
     }
-    
+
     public int getTCourses() {
-    	return numOfTCourses;
+        return numOfTCourses;
     }
-    
+
     public int getPointsReq() {
-    	return pointsNeeded;
+        return pointsNeeded;
     }
-    
+
     //three functions to set the values of a gpa object. the other two arent
     //needed as there should be no reason to set those manually
     public void setCGPA(float gpa) {
-    	cgpa = gpa;
+        cgpa = gpa;
     }
-    
-    public void setTBTCourses(int courseNum) {
-    	numOfTBTCourses = courseNum;
+
+    public void setCourses(int courseNum) {
+        numOfCourses = courseNum;
     }
-    
+
     public void setTCourses(int courseNum) {
-    	numOfTCourses = courseNum;
+        numOfTCourses = courseNum;
     }
 }
--- src/Permutation.java
+++ src/Permutation.java
@@ -4,61 +4,61 @@ import java.util.Stack;
 
 public class Permutation  {
     /*TODO:
-    *1)Apply a specific permutation algorithm with constraint in accordance to the requirements (Expected overall GPA)
-    * */
+     *1)Apply a specific permutation algorithm with constraint in accordance to the requirements (Expected overall GPA)
+     * */
     final int[] GRADEVALUES={7,6,5,4,0};
     int totalPointsReqd;
     int numOfCourses;
 
     List Permutation(int numOfCourses,int totalPointsReqd){
-      this.numOfCourses= numOfCourses;
-      this.totalPointsReqd=totalPointsReqd;
-      return getPermutation();
-   }
+        this.numOfCourses= numOfCourses;
+        this.totalPointsReqd=totalPointsReqd;
+        return getPermutation();
+    }
 
     /**
      * Obtains all valid permutation and returns it as an Arraylist
      * with the help of Stacks
      * @Return: An array list of all required permutation such that each permutation is ordered as [HDs,Ds,CRs,Ps,Fs]
      * */
-   public List getPermutation(){
+    public List getPermutation(){
 
-       int points=0;
-       int sumOfStack=0;
-       int[] trackers={0,0,0,0};
-       Stack<Integer> stack=new Stack();
-       ArrayList permutations=new ArrayList();
-       stack.push(trackers[0]);
-       while(!stack.empty()){
-           if(stack.size()==4){
-               stack.push(numOfCourses-sumOfStack); //calculating remaining left over
-               if(totalPointsReqd==points){
-                   permutations.add(stack.toArray()); //Converting  stack to array for the sake of the test , can be optimised later
-               }
-               sumOfStack-=stack.pop()-stack.pop();// 5th  & 4th element being removed from stack & subtracted at the same time
-           }
-           else{
-               int val=trackers[stack.size()-1];
-               if(val!=numOfCourses-sumOfStack){
-                   points+=val*GRADEVALUES[stack.size()-1];//updating points in accordance
-                   sumOfStack+=val;  //updating sumOfStack
-                   stack.push(val);
-                   trackers[stack.size()-1]++;
-               }
-               else{
-                   trackers[stack.size()-1]=0;
-                   if(stack.size()==1){
-                       stack.clear();
-                   }
-                   else {
-                       points -= stack.pop() * GRADEVALUES[stack.size() - 1];//updating points
-                   }
-               }
+        int points=0;
+        int sumOfStack=0;
+        int[] trackers={0,0,0,0};
+        Stack<Integer> stack=new Stack();
+        ArrayList permutations=new ArrayList();
+        stack.push(trackers[0]);
+        while(!stack.empty()){
+            if(stack.size()==4){
+                stack.push(numOfCourses-sumOfStack); //calculating remaining left over
+                if(totalPointsReqd==points){
+                    permutations.add(stack.toArray()); //Converting  stack to array for the sake of the test , can be optimised later
+                }
+                sumOfStack-=stack.pop()-stack.pop();// 5th  & 4th element being removed from stack & subtracted at the same time
+            }
+            else{
+                int val=trackers[stack.size()-1];
+                if(val!=numOfCourses-sumOfStack){
+                    points+=val*GRADEVALUES[stack.size()-1];//updating points in accordance
+                    sumOfStack+=val;  //updating sumOfStack
+                    stack.push(val);
+                    trackers[stack.size()-1]++;
+                }
+                else{
+                    trackers[stack.size()-1]=0;
+                    if(stack.size()==1){
+                        stack.clear();
+                    }
+                    else {
+                        points -= stack.pop() * GRADEVALUES[stack.size() - 1];//updating points
+                    }
+                }
 
-           }
-       }
+            }
+        }
         return permutations;
-   }
+    }
 
 
 }

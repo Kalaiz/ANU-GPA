337cdd782916e99b63aafbb7fcdb6e166568bcc6
u6555407 <u6555407@anu.edu.au>
2019-04-20 09:36:16 +1000

    Home Screen Animation Improved

--- ANU_GPA/app/src/main/java/com/example/ANU_GPA/Home.java
+++ ANU_GPA/app/src/main/java/com/example/ANU_GPA/Home.java
@@ -16,6 +16,11 @@ import android.widget.LinearLayout;
 
 public class Home extends AppCompatActivity {
 
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,106 +28,15 @@ public class Home extends AppCompatActivity {
         final LinearLayout gpaCalcLinearLayout = (LinearLayout) findViewById(R.id.linearLayout1);
         final LinearLayout permutationLinearLayout=(LinearLayout) findViewById(R.id.linearLayout2);
         final LinearLayout settingsLinearLayout=(LinearLayout) findViewById(R.id.linearLayout3);
-        final AnimatorSet scaleIn =new AnimatorSet();
-        final AnimatorSet scaleOut=new AnimatorSet();
-        final AnimatorSet effect=new AnimatorSet();
+        //For extensibility
 
 
-       final  ObjectAnimator anim= ObjectAnimator.ofFloat(gpaCalcLinearLayout, "rotationY", 360f)
-                ;
+        Thread t = new Thread(){
 
-        final  ObjectAnimator anim2= ObjectAnimator.ofFloat(gpaCalcLinearLayout, "rotationY", -360f)
-                ;
-
-
-        final  ObjectAnimator scaleX= ObjectAnimator.ofFloat(gpaCalcLinearLayout, "scaleX", 0.9f);
-
-        final  ObjectAnimator scaleY= ObjectAnimator.ofFloat(gpaCalcLinearLayout, "scaleY", 0.9f);
-
-        final  ObjectAnimator originalScaleX= ObjectAnimator.ofFloat(gpaCalcLinearLayout, "scaleX", 1);
-
-        final  ObjectAnimator originalScaleY= ObjectAnimator.ofFloat(gpaCalcLinearLayout, "scaleY", 1);
-
-
-        scaleIn.setDuration(1500);
-        scaleOut.setDuration(1500);
-        scaleIn.playTogether(scaleX,scaleY);
-        scaleOut.playTogether(originalScaleX,originalScaleY);
-        effect.playSequentially(scaleIn,scaleOut);
-        effect.start();
-
-        effect.addListener(new AnimatorListenerAdapter() {
-
-                               @Override
-                               public void onAnimationEnd(Animator animation) {
-                                   effect.start();
-                               }
-                           });
-
-
-
-//        ObjectAnimator.ofFloat(gpaCalcLinearLayout, "scaleY", .75f)
-//        .setDuration(2000)
-//                .start();
-//        new Handler().postDelayed(new Runnable() {
-//                                      @Override
-//                                      public void run() {
-//                                          ObjectAnimator.ofFloat(gpaCalcLinearLayout, "scaleX", 1f)
-//                                                  .setDuration(2000)
-//                                                  .start();
-//                                          ObjectAnimator.ofFloat(gpaCalcLinearLayout, "scaleY", 1f)
-//                                                  .setDuration(2000)
-//                                                  .start();
-//                                      }
-//                                  },2000);
-
-    /*    Animator.AnimatorListener animatorListener
-                = new Animator.AnimatorListener() {
-
-            public void onAnimationStart(Animator animation) {
-
-            }
-
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-
-            public void onAnimationEnd(Animator animation) {
-                scaleOut.start();
-            }
-
-            public void onAnimationCancel(Animator animation) {
-
-            }
         };
 
-        Animator.AnimatorListener animatorListener2
-                = new Animator.AnimatorListener() {
 
-            public void onAnimationStart(Animator animation) {
-
-            }
-
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-
-            public void onAnimationEnd(Animator animation) {
-                scaleIn.start();
-            }
-
-            public void onAnimationCancel(Animator animation) {
-
-            }
-        };
-
-        scaleIn.start();
-        scaleOut.start();
-        scaleIn.addListener(animatorListener);
-        scaleOut.addListener(animatorListener2);
-*/
-
-
+        effect(gpaCalcLinearLayout);
 
         gpaCalcLinearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -151,5 +65,33 @@ public class Home extends AppCompatActivity {
                 startActivity(intent);
             }
     });
+    }
+
+
+
+
+    public void effect(View view){
+        final AnimatorSet scaleIn =new AnimatorSet();
+        final AnimatorSet scaleOut=new AnimatorSet();
+        final AnimatorSet effect=new AnimatorSet();
+        final  ObjectAnimator scaleX= ObjectAnimator.ofFloat(view, "scaleX", 0.95f);
+        final  ObjectAnimator scaleY= ObjectAnimator.ofFloat(view, "scaleY", 0.95f);
+        final  ObjectAnimator originalScaleX= ObjectAnimator.ofFloat(view, "scaleX", 1);
+        final  ObjectAnimator originalScaleY= ObjectAnimator.ofFloat(view, "scaleY", 1);
+
+        scaleIn.setDuration(2000);
+        scaleOut.setDuration(2000);
+        scaleIn.playTogether(scaleX,scaleY);
+        scaleOut.playTogether(originalScaleX,originalScaleY);
+        effect.playSequentially(scaleIn,scaleOut);
+        effect.start();
+
+        effect.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                effect.start();
+            }
+        });
     }
 }

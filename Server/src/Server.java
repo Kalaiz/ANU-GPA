13886ee98c30bdf26b7d103be48743bb2cc34460
u6555407 <u6555407@anu.edu.au>
2019-04-20 06:23:41 +1000

    Server:Added text file for storage & made facilities to keep track of the date & time

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.*;
+
+
 
 public class Server {
     public static void main(String[] args) throws Exception{
@@ -13,7 +16,8 @@ public class Server {
         System.out.println("Server has connected with a client");
         InputStreamReader streamReader =new InputStreamReader(listeningSocket.getInputStream());
         BufferedReader br=new BufferedReader(streamReader);
-        String listeningSocketData="";
+        String time = LocalTime.now().toString().substring(0,5);
+        String listeningSocketData= "Feedback received on " + LocalDate.now()+ " at " + time +"\n";
         String temp="";
         try{
             while(true){
@@ -28,6 +32,7 @@ public class Server {
         catch (IOException i){
 
         }
+
 
         System.out.println("Server has received : " + listeningSocketData);
 
